25efee473c8681d05ac14e176bb6bcaf206a2b52
Vitor <vitorlara2112@gmail.com>
2022-04-28 08:53:34 -0300

    estrutura básica

---
+++ parte1/Pessoa.py
@@ -0,0 +1,9 @@
+class Pessoa():
+    def __init__(self, nome) -> None:
+        self._nome = nome # aqui ficam os atributos
+        
+
+
+    # aqui os métodos
+    # def metodo(parametros):
+        # código
